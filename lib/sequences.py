480462aea533a45dbbe5270a27af738ef24b0152
VictorIgati <victor.igati@student.moringaschool.com>
2025-02-11 17:41:23 +0300

    fibonnacci done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
-#!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence=[]
+
+    while len(fibonacci_sequence) < length:
+        if len(fibonacci_sequence) == 0:
+            fibonacci_sequence.append(0)
+        elif len(fibonacci_sequence) == 1:
+            fibonacci_sequence.append(1)
+        else:
+            fibonacci_sequence.append(fibonacci_sequence[-1] + fibonacci_sequence[-2])
+
+    print(fibonacci_sequence)            
