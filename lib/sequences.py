954d57eac04a8ec47df2d3dd5876d0445d25e945
VictorIgati <victor.igati@student.moringaschool.com>
2025-03-03 16:34:23 +0300

    done this lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,6 +1,11 @@
-
 def print_fibonacci(length):
-    fibonacci_sequence=[]
+    """
+    Prints the Fibonacci sequence up to the specified length.
+
+    Parameters:
+    length (int): The number of elements in the Fibonacci sequence to print.
+    """
+    fibonacci_sequence = []
 
     while len(fibonacci_sequence) < length:
         if len(fibonacci_sequence) == 0:
@@ -10,4 +15,4 @@ def print_fibonacci(length):
         else:
             fibonacci_sequence.append(fibonacci_sequence[-1] + fibonacci_sequence[-2])
 
-    print(fibonacci_sequence)            
+    print(fibonacci_sequence)
